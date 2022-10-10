65bb5fc1922133d18365e2e20f867a10b19cab1c
Conoisseur <97054370+Conoisseur@users.noreply.github.com>
2022-10-10 15:33:24 +0100

    added solution
    added solution

--- frequencies.py
+++ frequencies.py
@@ -2,6 +2,24 @@
 """ENTER YOUR SOLUTION HERE!"""
 
 def frequencies(items):
+    """
+    algorithm:
+    convert every item in list to a string if its not already
+    count number of each item in list
+        Can use a brute force method for this
+    add each item to dict
+    :param items:
+    :return:
+    """
+
     frequencies = {}
     # Your code goes here
+    for item in items:
+        if type(item) != "str":
+            item += ""
+
+    for item in items:
+        frequencies[item] = items.count(item)
+        items.remove(item)
+
     return frequencies
